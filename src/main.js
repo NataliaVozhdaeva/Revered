feb93371ae2ae1a3a56f57b051982bbfb84f6e6c
NataliaVozhdaeva <LomzNat@yandex.ru>
2024-01-30 21:01:52 +0400

    refactor: cards manipulation

--- src/main.js
+++ src/main.js
@@ -22,28 +22,45 @@ document.addEventListener('click', (e) => {
   }
 });
 
-//hover for cards
+//cards manipulation
 
 const cards = document.querySelectorAll('.offer-item');
 const cardBtns = document.querySelectorAll('.offer-btn');
+const currencyArray = ['\u0024', '\u20BD', '\u20AC'];
+const currencyPoints = document.querySelectorAll('.offer-currency');
 
 const makeContent = (el) => {
   el.querySelector('.offer-btn').textContent = el.classList.contains('active') ? 'Get Started' : 'Start Free Trial';
 };
 
+let currCount = 0;
+
+const changeCurrency = () => {
+  currCount === currencyArray.length - 1 ? (currCount = 0) : currCount++;
+  currencyPoints.forEach((el) => (el.textContent = currencyArray[currCount]));
+};
+
 cards.forEach((item) => makeContent(item));
 
-const makeActive = (e) => {
-  cards.forEach((item) => {
-    item.classList.remove('active');
-  });
-  e.currentTarget.classList.add('active');
-  cards.forEach((item) => makeContent(item));
+const handleChanges = (e) => {
+  switch (true) {
+    case e.target.classList.contains('offer-currency'):
+      changeCurrency();
+      break;
+
+    default:
+      cards.forEach((item) => {
+        item.classList.remove('active');
+      });
+      e.currentTarget.classList.add('active');
+      cards.forEach((item) => makeContent(item));
+      break;
+  }
 };
 
 cards.forEach((item) => {
-  item.addEventListener('click', makeActive); //mouseover as a variant
-  item.addEventListener('touchstart', makeActive);
+  item.addEventListener('click', handleChanges);
+  item.addEventListener('touchstart', handleChanges);
 });
 
 // smooth appearance
@@ -55,7 +72,7 @@ const showCard = (el) => {
 window.addEventListener('load', () => {
   document.querySelector('.loader-box').style.display = 'none';
 
-  count = 0;
+  let count = 0;
 
   const intervalId = setInterval(function () {
     showCard(cards[count]);
